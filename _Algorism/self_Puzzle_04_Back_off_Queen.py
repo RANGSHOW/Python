68a8de618d9ede96144484a4f73197af10c4b38a
RANGSHOW <53436759+RANGSHOW@users.noreply.github.com>
2020-04-14 01:20:06 +0900

    2020/04/14

--- _Algorism/self_Puzzle_04_Back_off_Queen.py
+++ _Algorism/self_Puzzle_04_Back_off_Queen.py
@@ -6,26 +6,51 @@ class Queen(object):
         self.pos = pos
         board[self.pos[0]][self.pos[1]] = 1   #Queen의 위치를 초기화와 함께 board에 '1'로 표시
         
+        for i in range(queen):
+            board[self.pos[0]][i] += 1
+            board[i][self.pos[1]] += 1
+            
+        #대각선 추가해야해요
+    
+    
+    
 def show_board():
-    print('---------------')
     for i in range(queen):
         for j in range(len(board[i])):
-            print(board[i, j], end=' ')
+            print(board[i][j], end=' ')
         print('')
-    print('---------------')
-
-
+    print('\n')
     
     
+def return_possible_pos() -> tuple:
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                return (i, j)
+            else:
+                return None
+
+def make_queen():
+    for queen in queen_list:
+        if queen != 0:
+            board[queen.pos[0]][queen.pos[1]] = 'Q' #chr(9813)
+        else:
+            break
     
 if __name__ == "__main__":
     queen = 8
-    board = np.zeros((queen, queen), dtype='int')
+    board = np.zeros((queen, queen), dtype='int').tolist()
     queen_list = [0] * queen
-    
-    queen_list[0] = Queen((randint(0, queen - 1), randint(0, queen - 1)))
-    
+#     queen_list[0] = Queen((randint(0, queen - 1), randint(0, queen - 1)))
+    queen_list[0] = Queen((2, 2))
+    make_queen()
     show_board()
-
     
+    
+#for i in range(1, 8):
+#    if return_possible_pos:
+#        queen_list[i] = Queen(return_possible_pos())
+#    else:
+#        print('Queen: "There is no place to go!"')
+#    board()
 
